3ff8e2c4b317c46bc3f79b49f55893aaa828e3ff
Justin <justincase25154@gmail.com>
2023-12-20 13:19:40 +0300

    first commit

---
+++ OOPGIT.py
@@ -0,0 +1,11 @@
+'''=+-_This is class for getting students info_-+='''
+class Talaba:
+    def __init__(self, ism, familiya, tugilgan_yil) -> None:
+        self.ism = ism
+        self.familiya = familiya
+        self.tugilgan_yil = tugilgan_yil
+
+    def ret(self, ism, familiya, tugilganYil):
+        print(ism, familiya, tugilganYil)
+    
+    d  =  3
